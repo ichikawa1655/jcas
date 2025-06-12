a9dbb70e90a84a1fd7faf35fbc9d63ca53307a03
市川 <ichikawa053107@gmail.com>
2025-06-12 17:10:56 +0900

    header編集

--- header.php
+++ header.php
@@ -14,9 +14,9 @@
 <body>
 <div class="wrapper">
     <!-- ページのトップに置いておくヘッダー -->
-    <header id="TOP-HEADER" class="top-header">
-        <h1><a href="<?php echo esc_url(home_url('//')); ?>" class="left-logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="JCAS Airways"></a></h1>
-        <ul class="right-buttons">
+    <header id="TOP_HEADER" class="top_header">
+        <h1><a href="<?php echo esc_url(home_url('//')); ?>" class="left_logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="JCAS Airways"></a></h1>
+        <ul class="right_buttons">
             <li><a href="<?php echo esc_url(home_url('//')); ?>">私たちについて</a></li>
             <li><a href="<?php echo esc_url(home_url('//')); ?>">地域連携</a></li>
             <li><a href="<?php echo esc_url(home_url('//')); ?>">会社概要</a></li>
@@ -26,50 +26,50 @@
         </ul>
     </header>
     <!-- スクロール時に出てくる固定ヘッダー -->
-    <header class="scroll-header">
-        <h1><a href="<?php echo esc_url(home_url('//')); ?>" class="left-logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="JCAS Airways"></a></h1>
-        <input type="checkbox" class="menu-btn" id="menu-btn">
-        <label for="menu-btn" class="menu-icon"><span class="navicon"></span></label>
+    <header class="scroll_header">
+        <h1><a href="<?php echo esc_url(home_url('//')); ?>" class="left_logo"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="JCAS Airways"></a></h1>
+        <input type="checkbox" class="menu_btn" id="menu_btn">
+        <label for="menu_btn" class="menu_icon"><span class="navicon"></span></label>
         <div class="menu">
             <ul class="menu-buttons">
                 <li>
                     <a href="<?php echo esc_url(home_url('//')); ?>">
-                        <div class="text-box">
+                        <div class="text_box">
                             <p class="en">About us</p>
                             <p class="button_title">私たちについて</p>
                         </div>
                         <i class="icon-main-arrow"></i>
                     </a>
                     <a href="<?php echo esc_url(home_url('//')); ?>">
-                        <div class="text-box">
+                        <div class="text_box">
                             <p class="en">Company</p>
                             <p class="button_title">会社概要</p>
                         </div>
                         <i class="icon-main-arrow"></i>
                     </a>
                     <a href="<?php echo esc_url(home_url('//')); ?>">
-                        <div class="text-box">
+                        <div class="text_box">
                             <p class="en">Business</p>
                             <p class="button_title">事業紹介</p>
                         </div>
                         <i class="icon-main-arrow"></i>
                     </a>
                     <a href="<?php echo esc_url(home_url('//')); ?>">
-                        <div class="text-box">
+                        <div class="text_box">
                             <p class="en">Co-Creation Ares</p>
                             <p class="button_title">地域連携</p>
                         </div>
                         <i class="icon-main-arrow"></i>
                     </a>
                     <a href="<?php echo esc_url(home_url('//')); ?>">
-                        <div class="text-box">
+                        <div class="text_box">
                             <p class="en">News</p>
                             <p class="button_title">ニュース</p>
                         </div>
                         <i class="icon-main-arrow"></i>
                     </a>
                     <a href="<?php echo esc_url(home_url('//')); ?>">
-                        <div class="text-box">
+                        <div class="text_box">
                             <p class="en">Recruitment</p>
                             <p class="button_title">採用情報</p>
                         </div>
